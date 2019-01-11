c12189163dbc4c7cefba4a202165848ffbd7a05b
Ellis Percival <flyte@failcode.co.uk>
2019-01-11 18:23:03 +0000

    Hook up mqtt

--- mqtt.py
+++ mqtt.py
@@ -4,6 +4,7 @@ import yaml
 import sys
 import socket
 import json
+import re
 from time import sleep
 from functools import partial
 
@@ -98,6 +99,44 @@ def init_mqtt(config):
         status_topic,
         config["status_payload_dead"])
 
+    client.on_log = on_log
+
+    return client
+
+
+if __name__ == "__main__":
+    p = argparse.ArgumentParser()
+    p.add_argument("config")
+    args = p.parse_args()
+
+    with open(args.config) as f:
+        config = yaml.load(f)
+    validator = ConfigValidator(CONFIG_SCHEMA)
+    if not validator.validate(config):
+        _LOG.error(
+            "Config did not validate:\n%s",
+            yaml.dump(validator.errors))
+        sys.exit(1)
+    config = validator.normalized(config)
+
+    client = init_mqtt(config["mqtt"])
+
+    topic_prefix = config["mqtt"]["topic_prefix"]
+    vehicles_by_mac = {x['mac'].lower(): x for x in config['anki']['vehicles']}
+    vehicles_by_name = {x['name']: x for x in config['anki']['vehicles']}
+
+    def publish_params(topic, addr, params=None):
+        veh = vehicles_by_mac[addr.lower()]
+        client.publish(
+            '%s/%s/%s' % (topic_prefix, veh['name'], topic),
+            json.dumps(params) if params is not None else None,
+            qos=2)
+
+    vehicles = {}
+    loc_change_callback = partial(publish_params, 'location')
+    transition_callback = partial(publish_params, 'transition')
+    pong_callback = partial(publish_params, 'pong')
+
     def on_conn(client, userdata, flags, rc):
         """
         On connection to MQTT, subscribe to the relevant topics.
@@ -114,10 +153,20 @@ def init_mqtt(config):
         if rc == 0:
             _LOG.info(
                 "Connected to the MQTT broker with protocol v%s.",
-                config["protocol"])
+                config['mqtt']['protocol'])
+            print(vehicles)
+            for veh in vehicles.values():
+                for suffix in ('speed', 'lane', 'ping'):
+                    topic = '%s/%s/%s' % (
+                        topic_prefix,
+                        veh['name'],
+                        suffix
+                    )
+                    client.subscribe(topic, qos=1)
+                    _LOG.info('Subscribed to topic: %r', topic)
             client.publish(
-                status_topic,
-                config["status_payload_running"],
+                config["mqtt"]["status_topic"],
+                config['mqtt']['status_payload_running'],
                 qos=1,
                 retain=True)
         elif rc == 1:
@@ -141,58 +190,53 @@ def init_mqtt(config):
                 "Not authorised to connect to MQTT broker.")
             sys.exit(1)
 
+
+    def on_msg(client, userdata, msg):
+        """
+        On reception of MQTT message, perform the relevant action to a vehicle
+        :param client: Connected MQTT client instance
+        :type client: paho.mqtt.client.Client
+        :param userdata: User data (any data type)
+        :param msg: Received message instance
+        :type msg: paho.mqtt.client.MQTTMessage
+        :return: None
+        :rtype: NoneType
+        """
+        print(msg)
+        try:
+            match = re.match(r'^%s/(.+?)/(.+?)$' % topic_prefix, msg.topic)
+            if match is None:
+                _LOG.warning('Could not parse topic: %s', msg.topic)
+                return
+            veh_name, action = match.groups()
+            veh = vehicles_by_name[veh_name]
+            if action == 'speed':
+                pass
+            elif action == 'lane':
+                pass
+            elif action == 'ping':
+                veh.ping()
+
+        except Exception:
+            _LOG.exception('Exception while handing received MQTT message:')
+
     client.on_connect = on_conn
-    client.on_log = on_log
-
-    return client
-
-
-if __name__ == "__main__":
-    p = argparse.ArgumentParser()
-    p.add_argument("config")
-    args = p.parse_args()
-
-    with open(args.config) as f:
-        config = yaml.load(f)
-    validator = ConfigValidator(CONFIG_SCHEMA)
-    if not validator.validate(config):
-        _LOG.error(
-            "Config did not validate:\n%s",
-            yaml.dump(validator.errors))
-        sys.exit(1)
-    config = validator.normalized(config)
-
-    client = init_mqtt(config["mqtt"])
+    client.on_message = on_msg
 
     try:
         client.connect(config["mqtt"]["host"], config["mqtt"]["port"], 60)
     except socket.error as err:
         _LOG.fatal("Unable to connect to MQTT server: %s" % err)
         sys.exit(1)
+
     client.loop_start()
-
-    topic_prefix = config["mqtt"]["topic_prefix"]
-    vehicles_by_mac = {x['mac'].lower(): x for x in config['anki']['vehicles']}
-    vehicles_by_name = {x['name']: x for x in config['anki']['vehicles']}
-
-    def publish_params(topic, addr, params=None):
-        veh = vehicles_by_mac[addr.lower()]
-        client.publish(
-            '%s/%s/%s' % (topic_prefix, veh['name'], topic),
-            json.dumps(params) if params is not None else None,
-            qos=2)
-
-    vehicles = {}
-    loc_change_callback = partial(publish_params, 'location')
-    transition_callback = partial(publish_params, 'transition')
-    pong_callback = partial(publish_params, 'pong')
 
     for vehicle in config['anki']['vehicles']:
         veh = Overdrive(vehicle['mac'])
         client.publish(
-            '%s/%s/connected' % (topic_prefix, vehicle['name']),
-            True,
-            qos=2
+        '%s/%s/connected' % (topic_prefix, vehicle['name']),
+        True,
+        qos=2
         )
         veh.setLocationChangeCallback(loc_change_callback)
         veh.setTransitionCallback(transition_callback)
